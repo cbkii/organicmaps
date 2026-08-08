bb53b64bf10a08ac6712b6b605dec7bce16cbf9e
cbkii <150587541+cbkii@users.noreply.github.com>
2026-08-09 00:24:09 +1000

    [android] Disable OSM upload scheduling in in-car flavour

---
+++ android/app/src/inCar/java/app/organicmaps/background/OsmUploadScheduler.java
@@ -0,0 +1,13 @@
+package app.organicmaps.background;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+
+/** No-op scheduler for the in-car flavour, which does not package WorkManager. */
+public final class OsmUploadScheduler
+{
+  private OsmUploadScheduler() {}
+
+  public static void schedule(@NonNull Context context)
+  {}
+}
