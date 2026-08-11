cb0fa6856f13a602b6cdf7f0d55b5d357441be3f
cbkii <150587541+cbkii@users.noreply.github.com>
2026-08-11 21:25:07 +1000

    [android] Format Driving View Drape dispatch
    Apply repository clang-format output to the small native Driving View bridge.
    
    Signed-off-by: ChatGPT <noreply@openai.com>

--- libs/drape_frontend/drape_engine_driving_view.cpp
+++ libs/drape_frontend/drape_engine_driving_view.cpp
@@ -8,10 +8,8 @@ void DrapeEngine::SetDrivingView(bool enabled, bool autoReturn, bool recenter)
 {
   m_threadCommutator->PostMessage(
       ThreadsCommutator::RenderThread,
-      make_unique_dp<NotifyRenderThreadMessage>(
-          [this, enabled, autoReturn, recenter](uint64_t)
-          { m_frontend->SetDrivingView(enabled, autoReturn, recenter); },
-          0 /* notifyId */),
+      make_unique_dp<NotifyRenderThreadMessage>([this, enabled, autoReturn, recenter](uint64_t)
+  { m_frontend->SetDrivingView(enabled, autoReturn, recenter); }, 0 /* notifyId */),
       MessagePriority::Normal);
 }
 }  // namespace df
