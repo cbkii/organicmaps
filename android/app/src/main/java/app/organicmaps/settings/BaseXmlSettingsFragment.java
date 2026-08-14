c5361c78a0012f4fc072dd87b31529c2faabb3c7
cbkii <150587541+cbkii@users.noreply.github.com>
2026-08-15 04:22:40 +1000

    [android] Normalise settings touch targets
    Give every XML preference row a shared 64dp minimum touch target while retaining the existing RecyclerView/inset scrolling path and unclipping bottom content.
    
    Signed-off-by: ChatGPT <noreply@openai.com>

--- android/app/src/main/java/app/organicmaps/settings/BaseXmlSettingsFragment.java
+++ android/app/src/main/java/app/organicmaps/settings/BaseXmlSettingsFragment.java
@@ -27,6 +27,7 @@ abstract class BaseXmlSettingsFragment extends PreferenceFragmentCompat
       throw new RuntimeException("Can't get preference by key: " + key);
     return pref;
   }
+
   @Override
   public void onCreatePreferences(Bundle bundle, String root)
   {
@@ -46,8 +47,28 @@ abstract class BaseXmlSettingsFragment extends PreferenceFragmentCompat
   {
     super.onViewCreated(view, savedInstanceState);
     view.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.bg_cards));
-    RecyclerView recyclerView = getListView();
+    final RecyclerView recyclerView = getListView();
+    recyclerView.setClipToPadding(false);
+    installTouchTargetPolicy(recyclerView);
     ViewCompat.setOnApplyWindowInsetsListener(recyclerView, new ScrollableContentInsetsListener(recyclerView));
+  }
+
+  private void installTouchTargetPolicy(@NonNull RecyclerView recyclerView)
+  {
+    final int minimumHeight = getResources().getDimensionPixelSize(R.dimen.automotive_preference_row_min_height);
+    for (int i = 0; i < recyclerView.getChildCount(); i++)
+      recyclerView.getChildAt(i).setMinimumHeight(minimumHeight);
+    recyclerView.addOnChildAttachStateChangeListener(new RecyclerView.OnChildAttachStateChangeListener() {
+      @Override
+      public void onChildViewAttachedToWindow(@NonNull View view)
+      {
+        view.setMinimumHeight(minimumHeight);
+      }
+
+      @Override
+      public void onChildViewDetachedFromWindow(@NonNull View view)
+      {}
+    });
   }
 
   protected SettingsActivity getSettingsActivity()
