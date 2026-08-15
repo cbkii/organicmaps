68de2329515db6f3b2ecd84068852b410442c337
cbkii <150587541+cbkii@users.noreply.github.com>
2026-08-15 16:27:44 +1000

    [android] Extend InCar dialog sizing edge coverage

--- android/app/src/test/java/app/organicmaps/incar/InCarDialogSizingTest.java
+++ android/app/src/test/java/app/organicmaps/incar/InCarDialogSizingTest.java
@@ -16,6 +16,20 @@ public class InCarDialogSizingTest
   }
 
   @Test
+  public void boundedSizeClampsNonPositiveAvailableToOne()
+  {
+    assertEquals(1, InCarDialogSizing.boundedSizePx(0, 288, 560, 0.5f));
+    assertEquals(1, InCarDialogSizing.boundedSizePx(-10, 288, 560, 0.5f));
+  }
+
+  @Test
+  public void boundedSizeHandlesFractionBounds()
+  {
+    assertEquals(288, InCarDialogSizing.boundedSizePx(1280, 288, 560, 0.0f));
+    assertEquals(560, InCarDialogSizing.boundedSizePx(1280, 288, 560, 1.0f));
+  }
+
+  @Test
   public void pickerHeightIsClampedWithoutExceedingUsableHeight()
   {
     assertEquals(560, InCarDialogSizing.boundedSizePx(720, 280, 560, 0.82f));
