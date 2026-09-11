64421b2e5c9e8b1bb8f0c7c04b7a98be8ea7405b
cbkii <150587541+cbkii@users.noreply.github.com>
2026-09-11 11:18:09 +1000

    Expand free-driving heuristic policy tests

--- libs/routing/routing_tests/free_driving_road_snap_policy_tests.cpp
+++ libs/routing/routing_tests/free_driving_road_snap_policy_tests.cpp
@@ -1,6 +1,9 @@
 #include "testing/testing.hpp"
 
+#include "routing/free_driving_area_context.hpp"
 #include "routing/free_driving_road_snap_policy.hpp"
+
+#include <cmath>
 
 namespace free_driving_road_snap_policy_tests
 {
@@ -25,19 +28,52 @@ UNIT_TEST(FreeDrivingRoadSnapPolicy_UsesAdaptiveAccuracyBands)
   TEST_EQUAL(GetAccuracyBand(MakeFix(10.0, 0.0, 1.0)), AccuracyBand::Unusable, ());
 }
 
-UNIT_TEST(FreeDrivingRoadSnapPolicy_SearchRadiusDoesNotExcludeLikelyRoad)
+UNIT_TEST(FreeDrivingRoadSnapPolicy_TightensAcceptanceAsAccuracyDegrades)
+{
+  TEST_ALMOST_EQUAL_ULPS(AcceptanceDistanceM(MakeFix(10.0, 8.0, 1.0)), 13.2, ());
+  TEST_ALMOST_EQUAL_ULPS(AcceptanceDistanceM(MakeFix(10.0, 25.0, 1.0)), 25.75, ());
+  TEST_ALMOST_EQUAL_ULPS(AcceptanceDistanceM(MakeFix(10.0, 50.0, 1.0)), 32.0, ());
+  TEST_LESS(StrongRoadDistanceM(MakeFix(10.0, 8.0, 1.0)), AcceptanceDistanceM(MakeFix(10.0, 8.0, 1.0)), ());
+}
+
+UNIT_TEST(FreeDrivingRoadSnapPolicy_SearchRadiusDoesNotExcludeRecoveryRoads)
 {
   TEST_ALMOST_EQUAL_ULPS(SearchRadiusM(MakeFix(10.0, 5.0, 1.0)), 20.0, ());
   TEST_ALMOST_EQUAL_ULPS(SearchRadiusM(MakeFix(10.0, 25.0, 1.0)), 42.5, ());
   TEST_ALMOST_EQUAL_ULPS(SearchRadiusM(MakeFix(10.0, 50.0, 1.0)), 50.0, ());
 }
 
-UNIT_TEST(FreeDrivingRoadSnapPolicy_HeadingWeightFallsAtLowSpeed)
+UNIT_TEST(FreeDrivingRoadSnapPolicy_ModerateAccuracyRequiresMoreAmbiguityMargin)
 {
-  TEST_ALMOST_EQUAL_ULPS(HeadingWeight(MakeFix(0.0, 8.0, 1.0)), 0.0, ());
-  TEST_ALMOST_EQUAL_ULPS(HeadingWeight(MakeFix(kDirectionUsefulMinSpeedMps, 8.0, 1.0)), 0.0, ());
-  TEST_GREATER(HeadingWeight(MakeFix(kCruiseSpeedMps, 8.0, 1.0)), 0.0, ());
-  TEST_ALMOST_EQUAL_ULPS(HeadingWeight(MakeFix(kDirectionFullWeightSpeedMps, 8.0, 1.0)), 1.0, ());
+  TEST_LESS(RequiredRunnerUpMargin(AccuracyBand::Good), RequiredRunnerUpMargin(AccuracyBand::Moderate), ());
+  TEST(IsUnambiguous(1.0, 1.4, AccuracyBand::Good), ());
+  TEST(!IsUnambiguous(1.0, 1.4, AccuracyBand::Moderate), ());
+  TEST(!IsUnambiguous(1.0, 10.0, AccuracyBand::Poor), ());
+}
+
+UNIT_TEST(FreeDrivingRoadSnapPolicy_HeadingWeightFallsAtLowSpeedAndConflict)
+{
+  TEST_ALMOST_EQUAL_ULPS(HeadingWeightForSpeed(0.0), 0.0, ());
+  TEST_ALMOST_EQUAL_ULPS(HeadingWeightForSpeed(kDirectionUsefulMinSpeedMps), 0.0, ());
+  TEST_GREATER(HeadingWeightForSpeed(kCruiseSpeedMps), 0.0, ());
+  TEST_ALMOST_EQUAL_ULPS(HeadingWeightForSpeed(kDirectionFullWeightSpeedMps), 1.0, ());
+  TEST_GREATER(HeadingAgreementFactor(20.0), HeadingAgreementFactor(70.0), ());
+  TEST_GREATER(HeadingAgreementFactor(70.0), HeadingAgreementFactor(150.0), ());
+}
+
+UNIT_TEST(FreeDrivingRoadSnapPolicy_InferMotionOnlyFromCredibleDisplacement)
+{
+  auto noSpeed = MakeFix(0.0, 8.0, 1.0, false);
+  TEST_ALMOST_EQUAL_ULPS(EffectiveSpeedMps(noSpeed, 10.0, 2.0), 5.0, ());
+  TEST_ALMOST_EQUAL_ULPS(EffectiveSpeedMps(noSpeed, 90.0, 2.0), 0.0, ());
+  TEST(HasEstablishedMotion(noSpeed, 6.0, 2.0), ());
+}
+
+UNIT_TEST(FreeDrivingRoadSnapPolicy_StationaryHoldRejectsJitterSwitches)
+{
+  auto stopped = MakeFix(0.0, 8.0, 1.0);
+  TEST(IsStationaryHold(stopped, 3.0, 1.0), ());
+  TEST(!IsStationaryHold(stopped, 10.0, 1.0), ());
 }
 
 UNIT_TEST(FreeDrivingRoadSnapPolicy_RecentDirectionWindowScalesWithSpeed)
@@ -55,27 +91,51 @@ UNIT_TEST(FreeDrivingRoadSnapPolicy_TemporalEvidenceRejectsBadClockIntervals)
   TEST_ALMOST_EQUAL_ULPS(TemporalEvidenceDeltaSeconds(100.0, 106.0), 0.0, ());
 }
 
-UNIT_TEST(FreeDrivingRoadSnapPolicy_ConnectedTurnsAreCheaperAtLowSpeed)
+UNIT_TEST(FreeDrivingRoadSnapPolicy_ContinuitySupportsTurnsAndUTurnsWithoutEquatingThem)
 {
   TEST_LESS(ContinuityPenalty(RoadRelation::SameDirectedEdge, true), ContinuityPenalty(RoadRelation::SameRoad, true),
             ());
   TEST_LESS(ContinuityPenalty(RoadRelation::SameRoad, true), ContinuityPenalty(RoadRelation::Connected, true), ());
-  TEST_LESS(ContinuityPenalty(RoadRelation::Connected, true), ContinuityPenalty(RoadRelation::Unrelated, true), ());
+  TEST_LESS(ContinuityPenalty(RoadRelation::ReverseSameRoad, true), ContinuityPenalty(RoadRelation::Unrelated, true),
+            ());
   TEST_LESS(ContinuityPenalty(RoadRelation::Connected, true), ContinuityPenalty(RoadRelation::Connected, false), ());
 }
 
-UNIT_TEST(FreeDrivingRoadSnapPolicy_ParkingReleaseRequiresLowSpeedAndContext)
+UNIT_TEST(FreeDrivingRoadSnapPolicy_RoadClassSpeedPriorIsWeakAndOnlyForUnrelatedRoads)
 {
-  AreaContext surfaceParking;
-  surfaceParking.m_insideParking = true;
-  TEST(!CanEnterParkingFree(MakeFix(20.0 / 3.6, 8.0, 10.0), surfaceParking, 10.0, false), ());
-  TEST(!CanEnterParkingFree(MakeFix(10.0 / 3.6, 8.0, 10.0), surfaceParking, 2.9, false), ());
-  TEST(CanEnterParkingFree(MakeFix(10.0 / 3.6, 8.0, 10.0), surfaceParking, 3.0, false), ());
+  auto fast = MakeFix(90.0 / 3.6, 8.0, 1.0);
+  RoadMetadata motorway;
+  motorway.m_class = RoadClass::Motorway;
+  RoadMetadata service;
+  service.m_class = RoadClass::Service;
 
-  AreaContext building;
-  building.m_insideLargeBuilding = true;
-  TEST(!CanEnterParkingFree(MakeFix(5.0 / 3.6, 8.0, 10.0), building, 6.0, false), ());
-  TEST(CanEnterParkingFree(MakeFix(5.0 / 3.6, 8.0, 10.0), building, 6.0, true), ());
+  TEST_ALMOST_EQUAL_ULPS(RoadClassSpeedPenalty(fast, motorway, RoadRelation::Unrelated), 0.0, ());
+  TEST_GREATER(RoadClassSpeedPenalty(fast, service, RoadRelation::Unrelated), 0.0, ());
+  TEST_ALMOST_EQUAL_ULPS(RoadClassSpeedPenalty(fast, service, RoadRelation::Connected), 0.0, ());
+}
+
+UNIT_TEST(FreeDrivingRoadSnapPolicy_ParkingEntranceIsHintNotReleaseAuthority)
+{
+  auto slow = MakeFix(10.0 / 3.6, 8.0, 10.0);
+  AreaContext entranceOnly;
+  entranceOnly.m_nearParkingEntrance = true;
+  TEST(!ParkingReleaseEligible(slow, entranceOnly, true, false, false), ());
+
+  AreaContext surface;
+  surface.m_insideParking = true;
+  TEST(ParkingReleaseEligible(slow, surface, false, false, false), ());
+}
+
+UNIT_TEST(FreeDrivingRoadSnapPolicy_MappedParkingAisleStaysUsefulUntilFinePositioning)
+{
+  AreaContext surface;
+  surface.m_insideParking = true;
+  auto traversing = MakeFix(10.0 / 3.6, 8.0, 10.0);
+  auto manoeuvring = MakeFix(5.0 / 3.6, 8.0, 10.0);
+
+  TEST(!ParkingReleaseEligible(traversing, surface, false, true, false), ());
+  TEST(ParkingReleaseEligible(manoeuvring, surface, false, true, false), ());
+  TEST(ParkingReleaseEligible(traversing, surface, false, true, true), ());
 }
 
 UNIT_TEST(FreeDrivingRoadSnapPolicy_StructuredParkingReleasesSooner)
@@ -84,10 +144,10 @@ UNIT_TEST(FreeDrivingRoadSnapPolicy_StructuredParkingReleasesSooner)
   surface.m_insideParking = true;
   AreaContext structured;
   structured.m_insideStructuredParking = true;
-  TEST_LESS(ParkingReleaseTimeSeconds(structured), ParkingReleaseTimeSeconds(surface), ());
+  TEST_LESS(ParkingReleaseTimeSeconds(structured, false), ParkingReleaseTimeSeconds(surface, false), ());
 }
 
-UNIT_TEST(FreeDrivingRoadSnapPolicy_OpenLandOnlyAcceleratesSustainedRelease)
+UNIT_TEST(FreeDrivingRoadSnapPolicy_OpenLandAcceleratesOnlySustainedRelease)
 {
   AreaContext none;
   AreaContext strong;
@@ -95,13 +155,10 @@ UNIT_TEST(FreeDrivingRoadSnapPolicy_OpenLandOnlyAcceleratesSustainedRelease)
   AreaContext weak;
   weak.m_insideWeakOpenArea = true;
 
-  TEST_LESS(OffRoadReleaseTimeSeconds(strong), OffRoadReleaseTimeSeconds(weak), ());
-  TEST_LESS(OffRoadReleaseTimeSeconds(weak), OffRoadReleaseTimeSeconds(none), ());
-  TEST_LESS(OffRoadReleaseDistanceM(strong), OffRoadReleaseDistanceM(weak), ());
-  TEST_LESS(OffRoadReleaseDistanceM(weak), OffRoadReleaseDistanceM(none), ());
-  TEST(!CanEnterOffRoadFree(strong, OffRoadReleaseTimeSeconds(strong) - 0.1, 100.0), ());
-  TEST(!CanEnterOffRoadFree(strong, 100.0, OffRoadReleaseDistanceM(strong) - 0.1), ());
-  TEST(CanEnterOffRoadFree(strong, OffRoadReleaseTimeSeconds(strong), OffRoadReleaseDistanceM(strong)), ());
+  TEST_LESS(OffRoadReleaseTimeSeconds(strong, true), OffRoadReleaseTimeSeconds(weak, true), ());
+  TEST_LESS(OffRoadReleaseTimeSeconds(weak, true), OffRoadReleaseTimeSeconds(none, true), ());
+  TEST_LESS(OffRoadReleaseDistanceM(strong, true), OffRoadReleaseDistanceM(weak, true), ());
+  TEST_LESS(OffRoadReleaseDistanceM(weak, true), OffRoadReleaseDistanceM(none, true), ());
 }
 
 UNIT_TEST(FreeDrivingRoadSnapPolicy_ContextRefreshIsBounded)
@@ -111,5 +168,21 @@ UNIT_TEST(FreeDrivingRoadSnapPolicy_ContextRefreshIsBounded)
   TEST(ShouldRefreshAreaContext(100.0, 102.0, 5.0), ());
   TEST(ShouldRefreshAreaContext(100.0, 101.0, 20.0), ());
   TEST(ShouldRefreshAreaContext(100.0, 99.0, 0.0), ());
+}
+
+UNIT_TEST(FreeDrivingRoadSnapPolicy_ParkingAreaConstraintPreservesInsidePosition)
+{
+  AreaContext context;
+  context.m_freeAreaTriangles = {{0.0, 0.0}, {1.0, 0.0}, {0.0, 1.0}};
+
+  m2::PointD constrained;
+  m2::PointD const inside{0.2, 0.2};
+  TEST(ConstrainPointToFreeArea(context, inside, constrained), ());
+  TEST_LESS((constrained - inside).SquaredLength(), 1e-12, ());
+
+  m2::PointD const outside{0.8, 0.8};
+  TEST(ConstrainPointToFreeArea(context, outside, constrained), ());
+  TEST_LESS(std::abs(constrained.x - 0.5), 1e-9, ());
+  TEST_LESS(std::abs(constrained.y - 0.5), 1e-9, ());
 }
 }  // namespace free_driving_road_snap_policy_tests
