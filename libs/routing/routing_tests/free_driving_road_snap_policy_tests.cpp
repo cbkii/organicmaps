34d34b4b228079741c0c126c30b853e4bb6c13fa
cbkii <150587541+cbkii@users.noreply.github.com>
2026-09-19 09:40:40 +1000

    Test finite no-runner-up sentinel

--- libs/routing/routing_tests/free_driving_road_snap_policy_tests.cpp
+++ libs/routing/routing_tests/free_driving_road_snap_policy_tests.cpp
@@ -4,6 +4,7 @@
 #include "routing/free_driving_road_snap_policy.hpp"
 
 #include <cmath>
+#include <limits>
 
 namespace free_driving_road_snap_policy_tests
 {
@@ -55,6 +56,7 @@ UNIT_TEST(FreeDrivingRoadSnapPolicy_ModerateAccuracyRequiresMoreAmbiguityMargin)
   TEST(IsUnambiguous(1.0, 1.4, AccuracyBand::Good), ());
   TEST(!IsUnambiguous(1.0, 1.4, AccuracyBand::Moderate), ());
   TEST(!IsUnambiguous(1.0, 10.0, AccuracyBand::Poor), ());
+  TEST(IsUnambiguous(1.0, std::numeric_limits<double>::max(), AccuracyBand::Good), ());
 }
 
 UNIT_TEST(FreeDrivingRoadSnapPolicy_HeadingWeightFallsAtLowSpeedAndConflict)
