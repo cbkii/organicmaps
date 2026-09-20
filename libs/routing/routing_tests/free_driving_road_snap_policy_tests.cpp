18ae7f3916e8cbb33c9b76481c709b981d882a19
cbkii <150587541+cbkii@users.noreply.github.com>
2026-09-21 04:57:05 +1000

    Assert free-driving sentinels stay finite

--- libs/routing/routing_tests/free_driving_road_snap_policy_tests.cpp
+++ libs/routing/routing_tests/free_driving_road_snap_policy_tests.cpp
@@ -55,6 +55,8 @@ UNIT_TEST(FreeDrivingRoadSnapPolicy_ModerateAccuracyRequiresMoreAmbiguityMargin)
   TEST(IsUnambiguous(1.0, 1.4, AccuracyBand::Good), ());
   TEST(!IsUnambiguous(1.0, 1.4, AccuracyBand::Moderate), ());
   TEST(!IsUnambiguous(1.0, 10.0, AccuracyBand::Poor), ());
+  TEST(std::isfinite(kNoRunnerUpScore), ());
+  TEST(std::isfinite(RequiredRunnerUpMargin(AccuracyBand::Unusable)), ());
   TEST(IsUnambiguous(1.0, kNoRunnerUpScore, AccuracyBand::Good), ());
 }
 
