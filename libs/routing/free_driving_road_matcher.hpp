2acd06d8decba9fb2684912f3c4a5479d30e9938
cbkii <150587541+cbkii@users.noreply.github.com>
2026-09-25 04:57:35 +1000

    Remove superseded parking-aisle evidence flag

--- libs/routing/free_driving_road_matcher.hpp
+++ libs/routing/free_driving_road_matcher.hpp
@@ -23,7 +23,6 @@ struct MatcherEvidence
   bool m_bestStrong = false;
   bool m_bestUnambiguous = false;
   bool m_bestBeatsCurrent = false;
-  bool m_bestParkingAisle = false;
   bool m_bestParkingRoad = false;
   bool m_stationaryHold = false;
   bool m_motionEstablished = false;
